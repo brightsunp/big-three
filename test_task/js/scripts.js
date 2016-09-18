d27f90ad6ce4bdb4ded607be27242cfd1d04a945
brightsunp <446025838@qq.com>
2016-09-18 16:30:44 +0800

    0918

--- test_task/js/scripts.js
+++ test_task/js/scripts.js
@@ -1,23 +1,40 @@
 var app = angular.module('testTask', []);
 
-app.run(function($rootScope) {
-	$rootScope.flag = false;
-});
+app.controller('MainCtrl', function($scope) {
+	$scope.$on("CasesShowChange", function(event, msg) {		
+		$scope.$broadcast("CasesShowChangeFromMain", msg);
+	});
+	$scope.$on("HistShowChange", function(event, msg) {
+		$scope.$broadcast("HistShowChangeFromMain", msg);
+	});
 
-app.controller('CloudCtrl', ['$http', '$scope', function($http, $scope) {
+}).controller('CloudCtrl', function($scope) {
 	$scope.searchCases = function() {
 		$modal.modal('hide');
 		$('#cloud_container').hide();
 		$('#right_png').removeClass('blur');
 		$('#cases_nav').removeClass('todo').addClass('finished');
-		$rootScope.flag = true;
+		$scope.$emit("CasesShowChange", 1);
 	};
 
-}]);
-
-app.controller('CasesCtrl', ['$http', '$scope', function($http, $scope) {
+}).controller('CasesCtrl', function($http, $scope) {
+	$scope.$on("CasesShowChangeFromMain", function(event, msg) {	
+		$scope.casesShow = msg;
+	});	
 	// get(url) url: route for .json file
 	$http.get('cases2.json').success(function(data) {
 		$scope.cases = data;
 	});
-}]);
+
+	$scope.submit = function() {
+		$('#cases_container').hide();
+		$('#run_png').removeClass('blur');
+		$('#run_nav').removeClass('todo').addClass('finished');
+		$scope.$emit("HistShowChange", 1);
+		
+	};
+}).controller('HistoryCtrl', function($scope) {
+	$scope.$on("HistShowChangeFromMain", function(event, msg) {
+		$scope.histShow = msg;
+	});
+});
